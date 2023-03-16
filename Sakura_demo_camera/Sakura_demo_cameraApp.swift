0d45193530292d9326bfc779c4b850821c0440c3
seiba-atsushi <riki21061216@gmail.com>
2023-03-16 16:24:49 +0900

    Initial Commit

---
+++ Sakura_demo_camera/Sakura_demo_cameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sakura_demo_cameraApp.swift
+//  Sakura_demo_camera
+//
+//  Created by 本村力希 on 2023/03/16.
+//
+
+import SwiftUI
+
+@main
+struct Sakura_demo_cameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
